a826a3480cc950e02d43dcac406517208d351129
I194 <i1948374@yandex.ru>
2021-08-17 16:20:48 +0300

    App component version 2

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AppHeader from '../AppHeader/AppHeader';
 import BurgerIngredients from '../BurgerIngredients/BurgerIngredients';
 import BurgerConstructor from '../BurgerConstructor/BurgerConstructor';
 import styles from './App.module.css';
 
+const API = 'https://norma.nomoreparties.space/api/ingredients ';
+
 function App() {
+
+  const [state, setState] = useState({
+    loading: false,
+    error: false,
+    data: []
+  })
+
+  useEffect(() => {
+    const getAPIdata = async () => {
+      setState({...state, loading: true, error: false});
+      try {
+        const res = await fetch(API);
+        const data = await res.json();
+        setState({...state, data: data, loading: false});
+      }
+      catch (e) {
+        setState({...state, error: true, loading: false});
+      }
+    }
+
+    getAPIdata();
+  }, []);
+
   return (
     <div className="App">
       <AppHeader />
       <div className={`${styles.containerMain}`}>
-        <BurgerIngredients />
-        <BurgerConstructor />
+        <BurgerIngredients data={state.data}/>
+        <BurgerConstructor data={state.data}/>
       </div>
     </div>
   );
